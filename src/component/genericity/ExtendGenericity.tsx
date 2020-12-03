1925dc54f86b27163eaddfabecb7b56355895cac
TTDrazy <kexin19991018@126.com>
2020-12-03 19:09:35 +0800

    补充部分泛型的扩展应用

--- src/component/genericity/ExtendGenericity.tsx
+++ src/component/genericity/ExtendGenericity.tsx
@@ -1,7 +1,44 @@
 import * as React from 'react'
 
-class Axios<T>{
-  
+interface IVO {}
+interface IDTO {
+  id: number
+}
+
+interface IValidatorModel {
+  validator(): boolean // 校验方法
+}
+
+interface IApi<V extends IVO, D extends IDTO> {
+  getAll(): V[]
+  getById(id: number): V
+  add(d: D): V
+  update(d: D): V
+  delete(id: number): V
+}
+
+abstract class ABSValidatorModel implements IValidatorModel {
+  validator(): boolean {
+    throw new Error('Method not implemented.')
+  }
+}
+
+abstract class ABSDTO extends ABSValidatorModel implements IDTO {
+  id: number
+}
+// api 的泛型抽象类
+abstract class ABSApi<V extends IVO, D extends IDTO> implements IApi<V, D> {
+  path: string
+  axios: Axios<D>
+  TController: Controller<D>
+}
+
+// axios 泛型方法类
+class Axios<T> {
+  get<T>(path: string, id?: number): any {}
+  post<T>(path: string, DTOobj: any) {}
+  put<T>(path: string, DTOobj: any) {}
+  delete<T>(path: string, id?: number) {}
 }
 
 export interface IExtendGenericityProps {}
@@ -10,7 +47,7 @@ export default class ExtendGenericity extends React.Component<IExtendGenericityP
   public render() {
     return (
       <div>
-        <hr/>
+        <hr />
         <h2>对于泛型的补充应用</h2>
         <div>针对 api 层，结合模拟处理过的 axios 进行使用</div>
         <div>此次补充针对泛型的实例化以及开闭原则的实现</div>
