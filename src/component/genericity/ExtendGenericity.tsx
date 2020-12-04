63e9a6ee475ef8b58164a449a43cd11db7d41664
TTDrazy <kexin19991018@126.com>
2020-12-04 17:39:19 +0800

    练习前几日学习的泛型的补充应用

--- src/component/genericity/ExtendGenericity.tsx
+++ src/component/genericity/ExtendGenericity.tsx
@@ -1,20 +1,33 @@
 import * as React from 'react'
 
+// 泛型的构造函数类型
+type Constructor<D> = new (...arg: any[]) => D
+
 interface IVO {}
 interface IDTO {
   id: number
 }
+
+interface IMenuVO extends IVO {
+  treeList: []
+}
+
+interface IMenuDTO extends IDTO {}
 
 interface IValidatorModel {
   validator(): boolean // 校验方法
 }
 
 interface IApi<V extends IVO, D extends IDTO> {
-  getAll(): V[]
-  getById(id: number): V
-  add(d: D): V
-  update(d: D): V
-  delete(id: number): V
+  getAll(): Promise<V[]>
+  getById(id: number): Promise<V>
+  add(d: D): Promise<V>
+  update(d: D): Promise<V>
+  delete(id: number): Promise<V>
+}
+
+interface IMenuAPI extends ABSApi<IMenuVO, IMenuDTO> {
+  getTree(): IMenuVO
 }
 
 abstract class ABSValidatorModel implements IValidatorModel {
@@ -24,27 +37,88 @@ abstract class ABSValidatorModel implements IValidatorModel {
 }
 
 abstract class ABSDTO extends ABSValidatorModel implements IDTO {
-  id: number
+  id: any
 }
-// api 的泛型抽象类
+// api 的泛型抽象类,实际上也是泛型抽象工厂
+// 之所以是抽象类是因为我不想让他能够实例化，而仅仅让它能够继承
 abstract class ABSApi<V extends IVO, D extends IDTO> implements IApi<V, D> {
   path: string
   axios: Axios<D>
-  TController: Controller<D>
+  TConstructor: Constructor<D>
+  constructor(path: string, newConstructor: Constructor<D>) {
+    this.path = path
+    this.axios = new Axios<D>()
+    this.TConstructor = newConstructor
+  }
+  //实现方法
+  async getAll(): Promise<V[]> {
+    return await this.axios.get(this.path)
+  }
+  async getById(id: number): Promise<V> {
+    return await this.axios.get(this.path + id)
+  }
+  async add(d: D): Promise<V> {
+    const dto = new this.TConstructor(d)
+    return await this.axios.post(this.path, dto)
+  }
+  async update(d: D): Promise<V> {
+    const dto = new this.TConstructor(d)
+    return await this.axios.put(this.path, dto)
+  }
+  async delete(id: number): Promise<V> {
+    return await this.axios.delete(this.path + id)
+  }
 }
 
 // axios 泛型方法类
 class Axios<T> {
-  get<T>(path: string, id?: number): any {}
-  post<T>(path: string, DTOobj: any) {}
-  put<T>(path: string, DTOobj: any) {}
-  delete<T>(path: string, id?: number) {}
+  get(path: string, id?: number): any {}
+  post(path: string, DTOobj: T): any {}
+  put(path: string, DTOobj: T): any {}
+  delete(path: string, id?: number): any {}
+}
+
+class MenuVO implements IMenuVO {
+  treeList
+  constructor(treeList: []) {
+    this.treeList = treeList
+  }
+}
+
+class MenuDTO extends ABSDTO implements IMenuDTO {
+  id: number
+  parentId: number | null
+  name: string
+  constructor(param?: { id?: number; parentId?: number; name?: string }) {
+    super()
+    this.id = param?.id ?? 0
+    this.parentId = param?.parentId ?? null
+    this.name = param?.name ?? ''
+  }
+}
+
+class MenuAPI extends ABSApi<MenuVO, MenuDTO> implements IMenuAPI {
+  constructor() {
+    super('/menu', MenuDTO)
+  }
+  getTree(): IMenuVO {
+    console.log('我返回了一个 tree')
+    const vo = new MenuVO([])
+    return vo
+  }
 }
 
 export interface IExtendGenericityProps {}
 
 export default class ExtendGenericity extends React.Component<IExtendGenericityProps> {
   public render() {
+    let menuApi = new MenuAPI()
+    menuApi.getAll()
+    menuApi.getById(2)
+    menuApi.add(new MenuDTO({ id: 5, parentId: 1, name: '哈哈' }))
+    menuApi.update(new MenuDTO({ id: 6, parentId: 2, name: '哈哈222' }))
+    menuApi.delete(3)
+    menuApi.getTree()
     return (
       <div>
         <hr />
